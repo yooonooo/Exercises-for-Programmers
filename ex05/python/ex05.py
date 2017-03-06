b9e8fcdbcae45dd657e962e45bfa6f260dac23db
ywkim <rpssltm23>
2017-03-06 23:33:38 +0900

    연습문제 5

---
+++ ex05/python/ex05.py
@@ -0,0 +1,7 @@
+first_number = int(input("What is the first number?"))
+second_number = int(input("What is the second number?"))
+
+print(f"{first_number} + {second_number} = {first_number + second_number}")
+print(f"{first_number} - {second_number} = {first_number - second_number}")
+print(f"{first_number} * {second_number} = {first_number * second_number}")
+print(f"{first_number} / {second_number} = {first_number / second_number}")
