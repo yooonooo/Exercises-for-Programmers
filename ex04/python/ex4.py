527a06f3976bf32f006d80404429d6d76744e73c
ywkim <rpssltm23>
2017-03-06 23:27:59 +0900

    연습문제 4

---
+++ ex04/python/ex4.py
@@ -0,0 +1,6 @@
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter a adjective: ")
+adverb = input("Enter a adverb: ")
+
+print(f"Do you {verb} your {adjective} {noun} {adverb}? That's hilarious!")
